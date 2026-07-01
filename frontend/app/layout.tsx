446097d15404c7b411fae702a304ea87b9003696
calebomondi <kalebmokua@gmail.com>
2026-07-01 22:42:40 +0300

    feat: wrap children in Providers component within RootLayout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider"
-
+import { Providers } from "@/components/providers";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -40,7 +40,9 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <Providers>
+              {children}
+            </Providers>
           </ThemeProvider>
       </body>
     </html>
